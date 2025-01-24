60c7b9298af32593ca7161af4ffeceda97f52ff9
s4lvi <salvicomputers@gmail.com>
2025-01-23 23:54:19 -0500

    update

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
+const path = require("path");
 const userRoutes = require("./routes/users");
 const unionRoutes = require("./routes/unions");
 dotenv.config();
@@ -20,6 +21,18 @@ app.use(
 
 app.use("/api/users", userRoutes);
 app.use("/api/unions", unionRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "..", "frontend", "build", "index.html")
+    );
+  });
+}
+
 mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
@@ -28,5 +41,5 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
